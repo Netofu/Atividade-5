e94bf1d45791f23bec9b4eed527eb02325f6aa4c
Netofu <98113246+Netofu@users.noreply.github.com>
2025-12-12 17:18:00 -0300

    Update Question.ts

--- src/entities/Question.ts
+++ src/entities/Question.ts
@@ -1,1 +1,17 @@
 
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class Question {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    questionText: string;
+
+    @Column("simple-array")
+    options: string[];
+
+    @Column()
+    correctAnswerIndex: number;
+}
